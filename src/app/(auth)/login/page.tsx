01014c13ca008fb0b31dd33c85662a8881e22c31
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-11-29 15:05:21 -0300

    Login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,44 @@
+'use client';
 import React from 'react';
+import { Button, Form } from 'antd';
+import Link from 'next/link';
+
+interface LoginProps {
+  email: string;
+  password: string;
+}
 
 export default function Login() {
+  const onFinish = (values: LoginProps) => {
+    console.log(values);
+  };
+
   return (
     <div className="flex justify-center h-screen items-center bg-primary">
-      <h1>Login</h1>
+      <div className="card p-5 w-[450px]">
+        <h1 className="text-xl">Jobs - Login</h1>
+        <hr />
+
+        <Form
+          layout="vertical"
+          className="flex flex-col gap-5"
+          onFinish={onFinish}
+        >
+          <Form.Item label="Email" name="email">
+            <input type="email" className="input" />
+          </Form.Item>
+
+          <Form.Item label="Password" name="password">
+            <input type="password" className="input" />
+          </Form.Item>
+
+          <Button type="primary" htmlType="submit" block>
+            Login
+          </Button>
+
+          <Link href="/register">Don{`'`}t have an account? Register</Link>
+        </Form>
+      </div>
     </div>
   );
 }
