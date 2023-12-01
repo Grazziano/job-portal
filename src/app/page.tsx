2bd8040bf9f3cadae99e9c201f1cbc01a24e46f7
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-12-01 10:06:52 -0300

    Getting started sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,7 @@
-import axios from 'axios';
-import { cookies } from 'next/headers';
+import React from 'react'
 
-export async function getUser() {
-  try {
-    const token = cookies().get('token');
-
-    const response = await axios.get(
-      'http://localhost:3000/api/users/currentuser',
-      {
-        headers: {
-          Cookie: `token=${token?.value}`,
-        },
-      }
-    );
-
-    return response.data.data;
-  } catch (error: any) {
-    console.log(error);
-  }
-}
-
-export default async function Home() {
-  const user: any = await getUser();
-
+export default function Home() {
   return (
-    <main>
-      <h1>Job Portal</h1>
-
-      <h1>Current User: {user && user.name}</h1>
-    </main>
-  );
+    <div>Home</div>
+  )
 }
--- src/components/LayoutProvider.tsx
+++ src/components/LayoutProvider.tsx
@@ -1,11 +1,15 @@
+'use client';
 import React from 'react';
 import { ConfigProvider } from 'antd';
+import { usePathname } from 'next/navigation';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function LayoutProvider({ children }: LayoutProps) {
+  const pathname = usePathname();
+
   return (
     <html lang="en">
       <body>
@@ -16,7 +20,17 @@ export default function LayoutProvider({ children }: LayoutProps) {
             },
           }}
         >
-          {children}
+          {/* if route is login or register, dont show layout */}
+          {pathname === '/login' || pathname === '/register' ? (
+            <div>{children}</div>
+          ) : (
+            <div className="layout-parent">
+              <div className="sidebar">
+                <span>sidebar</span>
+              </div>
+              <div className="body">{children}</div>
+            </div>
+          )}
         </ConfigProvider>
       </body>
     </html>
