19154d8ed791415ab3f184c77485c4b42092feea
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-12-01 09:24:02 -0300

    Get current user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+'use client';
+import { message } from 'antd';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export default function Home() {
+  const [user, setUser] = useState<any>(null);
+
+  const getUser = async () => {
+    try {
+      const response = await axios.get('/api/users/currentuser');
+      setUser(response.data.data);
+    } catch (error: any) {
+      message.error(error.response.data.message || error.message);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <main>
       <h1>Job Portal</h1>
+
+      <h1>Current User: {user && user.name}</h1>
     </main>
   );
 }
