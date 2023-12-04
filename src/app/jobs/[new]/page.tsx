951fdbfde0d90cceff121aa60626b28e5f36d24e
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-12-04 08:51:33 -0300

    Job Form

--- src/app/jobs/[new]/page.tsx
+++ src/app/jobs/[new]/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 import React from 'react';
 import PageTitle from '@/components/PageTitle';
-import { Button } from 'antd';
+import { Button, Form } from 'antd';
 import { useRouter } from 'next/navigation';
+import JobPostForm from '@/components/JobPostForm';
 
 export default function NewJob() {
   const router = useRouter();
@@ -11,10 +12,23 @@ export default function NewJob() {
     <div>
       <div className="flex justify-between items-center">
         <PageTitle title="Post New Job" />
-        <Button type="primary" onClick={() => router.back()}>
+        <Button type="default" onClick={() => router.back()}>
           Back
         </Button>
       </div>
+
+      <Form layout="vertical">
+        <JobPostForm />
+
+        <div className="flex justify-end items-center gap-3 my-3">
+          <Button type="default" onClick={() => router.back()}>
+            Cancel
+          </Button>
+          <Button type="primary" htmlType="submit">
+            Post Job
+          </Button>
+        </div>
+      </Form>
     </div>
   );
 }
