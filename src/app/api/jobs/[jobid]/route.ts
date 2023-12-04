8ad6c9cb4d10f4449caa38b1e8efcab91e982ef4
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-12-04 14:35:27 -0300

    Edit job

---
+++ src/app/api/jobs/[jobid]/route.ts
@@ -0,0 +1,42 @@
+import { connectDB } from '@/config/dbConfig';
+import { validateJWT } from '@/helpers/validateJWT';
+import Job from '@/models/jobModel';
+import { NextRequest, NextResponse } from 'next/server';
+
+connectDB();
+
+interface JobParamsProps {
+  params: {
+    jobid: string;
+  };
+}
+
+export async function GET(request: NextRequest, { params }: JobParamsProps) {
+  try {
+    validateJWT(request);
+    const job = await Job.findById(params.jobid);
+    return NextResponse.json({
+      message: 'Job fetched successfully',
+      data: job,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
+
+export async function PUT(request: NextRequest, { params }: JobParamsProps) {
+  try {
+    validateJWT(request);
+    const reqBody = await request.json();
+    const job = await Job.findByIdAndUpdate(params.jobid, reqBody, {
+      new: true,
+      runValidators: true,
+    });
+    return NextResponse.json({
+      message: 'Job updated successfully',
+      data: job,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
--- src/app/jobs/edit/[jobid]/page.tsx
+++ src/app/jobs/edit/[jobid]/page.tsx
@@ -1,7 +1,75 @@
-import React from 'react'
+'use client';
+import React, { useEffect, useState } from 'react';
+import PageTitle from '@/components/PageTitle';
+import { Button, Form, message } from 'antd';
+import { useParams, useRouter } from 'next/navigation';
+import JobPostForm from '@/components/JobPostForm';
+import { useDispatch } from 'react-redux';
+import { SetLoading } from '@/redux/loadersSlice';
+import axios from 'axios';
 
 export default function EditJob() {
+  const [jobData, setJobData] = useState<any>(null);
+
+  const router = useRouter();
+
+  const { jobid } = useParams();
+
+  const dispatch = useDispatch();
+
+  const onFinish = async (values: any) => {
+    try {
+      values._id = jobid;
+      dispatch(SetLoading(true));
+      const response = await axios.put(`/api/jobs/${jobid}`, values);
+      message.success(response.data.message);
+      router.push('/jobs');
+    } catch (error: any) {
+      message.error(error.message);
+    } finally {
+      dispatch(SetLoading(false));
+    }
+  };
+
+  const fetchJob = async () => {
+    try {
+      dispatch(SetLoading(true));
+      const response = await axios.get(`/api/jobs/${jobid}`);
+      setJobData(response.data.data);
+    } catch (error: any) {
+      message.error(error.message);
+    } finally {
+      dispatch(SetLoading(false));
+    }
+  };
+
+  useEffect(() => {
+    fetchJob();
+  }, []);
+
   return (
-    <div>EditJob</div>
-  )
+    jobData && (
+      <div>
+        <div className="flex justify-between items-center">
+          <PageTitle title="Edit Job" />
+          <Button type="default" onClick={() => router.back()}>
+            Back
+          </Button>
+        </div>
+
+        <Form layout="vertical" onFinish={onFinish} initialValues={jobData}>
+          <JobPostForm />
+
+          <div className="flex justify-end items-center gap-3 my-3">
+            <Button type="default" onClick={() => router.back()}>
+              Cancel
+            </Button>
+            <Button type="primary" htmlType="submit">
+              Update Job
+            </Button>
+          </div>
+        </Form>
+      </div>
+    )
+  );
 }
