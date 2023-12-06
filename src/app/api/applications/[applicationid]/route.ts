49c7479e94c17369244a6b94bdf1911596c12c27
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-12-06 15:07:04 -0300

    Nodemailer setup

--- src/app/api/applications/[applicationid]/route.ts
+++ src/app/api/applications/[applicationid]/route.ts
@@ -1,4 +1,5 @@
 import { connectDB } from '@/config/dbConfig';
+import { sendEmail } from '@/helpers/sendEmail';
 import { validateJWT } from '@/helpers/validateJWT';
 import Application from '@/models/applicationModel';
 import { NextRequest, NextResponse } from 'next/server';
@@ -22,7 +23,19 @@ export async function PUT(request: NextRequest, { params }: JobParamsProps) {
         new: true,
         runValidators: true,
       }
-    );
+    ).populate('user');
+
+    await sendEmail({
+      to: application?.user?.email,
+      subject: 'Your application status has been updated',
+      text: `Your application status has been updated to ${application?.status}`,
+      html: `<div>
+                <p>Your application status has been updated to ${application?.status}</p>
+                <br />
+                <p>Tank you for using Jobs Portal</p>
+              </div>`,
+    });
+
     return NextResponse.json({
       message: 'Application updated successfully',
       data: application,
---
+++ src/helpers/sendEmail.ts
@@ -0,0 +1,31 @@
+import nodemailer from 'nodemailer';
+
+interface EmailRequest {
+  to: string;
+  subject: string;
+  text: string;
+  html: string;
+}
+
+export const sendEmail = async ({ to, subject, text, html }: EmailRequest) => {
+  try {
+    const transporter = await nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      auth: {
+        user: process.env.AUTH_USER,
+        pass: process.env.AUTH_PASSWORD,
+      },
+    });
+
+    await transporter.sendMail({
+      from: 'Jobs Portal',
+      to: to,
+      subject: '',
+      text: text,
+      html: html,
+    });
+  } catch (error) {
+    return error;
+  }
+};
